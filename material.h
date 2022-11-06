91e8f7a46579505853447bfac50b3ee0692856d1
Sahil Srivastava <previouslysahil@gmail.com>
2022-11-06 15:39:28 -0600

    added roughness to metal

--- material.h
+++ material.h
@@ -41,13 +41,15 @@ class lambertian: public material {
 
 class metal: public material {
     public:
-        metal(const color& a): albedo(a) {}
+        metal(const color& a, double f): albedo(a), fuzz(f < 1 ? f : 1) {}
     
         virtual bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
             // For our metal materials we need to get the reflected vector
             vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
             // Here we actually create the ray with its origin and direction
-            scattered = ray(rec.p, reflected);
+            // here fuzziness double randomizes the reflected direction creates
+            // a 'fuzzy' effect on the reflection of our metal surface.
+            scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
             // Our color
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
@@ -55,6 +57,7 @@ class metal: public material {
     
     public:
         color albedo;
+        double fuzz;
 };
 
 #endif
